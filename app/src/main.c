81de102c3ddc95a803fb4f3b01813225972d65cd
cempod <cempod@gmail.com>
2024-11-21 14:17:17 +0300

    Add color palette button

--- app/src/main.c
+++ app/src/main.c
@@ -23,9 +23,32 @@ LOG_MODULE_REGISTER(app);
 #define DISPLAY_LED_NODE DT_ALIAS(backlight0)
 LV_FONT_DECLARE(monocraft);
 
+static uint32_t count;
+static uint8_t palette = LV_PALETTE_RED;
+
+lv_obj_t *temp_label;
+lv_obj_t *humi_label;
+
 static const struct gpio_dt_spec display_led = GPIO_DT_SPEC_GET(DISPLAY_LED_NODE, gpios);
 
-static uint32_t count;
+#define BUTTON0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(BUTTON0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(BUTTON0_NODE, gpios, {0});
+static struct gpio_callback button_cb_data;
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	if(palette<LV_PALETTE_GREY) {
+		palette++;
+	} else {
+		palette = LV_PALETTE_RED;
+	}
+	lv_obj_set_style_text_color(temp_label, lv_palette_main(palette), 0);
+	lv_obj_set_style_text_color(humi_label, lv_palette_main(palette), 0);
+}
 
 int main(void)
 {
@@ -40,11 +63,33 @@ int main(void)
 		if (ret < 0) {
 			return 0;
 		}
+	
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return 0;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return 0;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
 
 	char count_str[10] = {0};
 	const struct device *display_dev;
-	lv_obj_t *temp_label;
-	lv_obj_t *humi_label;
 
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	if (!device_is_ready(display_dev)) {
@@ -58,8 +103,8 @@ int main(void)
 	lv_obj_align(humi_label, LV_ALIGN_BOTTOM_MID, 0, -10);
 	lv_obj_set_style_text_font(temp_label, &monocraft, 0);
 	lv_obj_set_style_text_font(humi_label, &monocraft, 0);
-	lv_obj_set_style_text_color(temp_label, lv_palette_main(LV_PALETTE_CYAN), 0);
-	lv_obj_set_style_text_color(humi_label, lv_palette_main(LV_PALETTE_CYAN), 0);
+	lv_obj_set_style_text_color(temp_label, lv_palette_main(palette), 0);
+	lv_obj_set_style_text_color(humi_label, lv_palette_main(palette), 0);
 	lv_disp_set_bg_color(NULL, lv_color_make(0x40,0x40,0x40));
 
 	lv_task_handler();
